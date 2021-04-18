06906f71d2db17282a4552da018707e5f95ed261
Walayy <76750141+Walayy@users.noreply.github.com>
2021-04-18 23:22:18 +0200

    accès aux zones

--- src/entite.c
+++ src/entite.c
@@ -76,7 +76,7 @@ void creationAcces(){
     a1.z2.niveau2 = 0; /* Cela signifie : Niveau 2 de la zone 2 par exemple */
     a1.z2.niveau3 = 0;
 
-    a1.zone3 = 0;
+    a1.zone3 = 1;
     a1.z3.niveau1 = 0;
     a1.z3.niveau2 = 0;
     a1.z3.niveau3 = 0;
